9a7e0b765b4377b07278dcf87e2ac2ebd11d7ca6
flionay <angyi_jq@163.com>
2022-01-14 15:42:25 +0800

    20220114

---
+++ 01 基本数据类型/转换为boolean.js
@@ -0,0 +1,22 @@
+// 数值-boolean。除了0和nan 都是true
+var a=123;
+console.log(Boolean(a))
+
+
+//string--boolean
+var a="";
+console.log(Boolean(a)) //f
+
+var a=null;
+console.log(Boolean(a)) //f
+
+var a="hello";
+console.log(Boolean(a)) //t
+
+var a=undefined;
+console.log(Boolean(a)) //f 
+
+
+a = Number(a)
+console.log(a)
+
