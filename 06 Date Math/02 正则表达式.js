ea9a453844bf12f2a2fb05c0f0470fe88c93ac7c
flionay <angyi_jq@163.com>
2022-01-21 19:39:48 +0800

    20220122

--- 06 Date Math/02 正则表达式.js
+++ 06 Date Math/02 正则表达式.js
@@ -26,7 +26,31 @@ reg = /[^ab]/;
 console.log(reg.test("a"))
 
 // {n} 正好出现几次 {m,n} m-n次 {m,} m次以上
+// + 至少一个
+// * 一个或者多个
+// ？ 0或1个
 // 只对前一个内容起作用，想要成组 要用（）
 reg = /(ab){3}/;
 reg = /(ab){1,3}/; //1-3 次
 console.log(reg.test("abababb"))
+
+// 检查一个字符串是否以a开头 
+// ^表示开头 [^a] 表示除了
+// a$ 表示结尾
+
+regs = /^a/;
+rege = /a$/;
+console.log(rege.test("bcabca"));
+
+
+// 检查是否是手机号
+// 手机号的规则 
+//1， 11位 
+//2，第一位 1 开头 
+//3. 第二位 3-9 任意数字
+//4. 三位以后 任意数字9字
+
+var phoneStr = "13834568967";
+regPhone = /^1[3-9][0-9]{9}$/;
+console.log(regPhone.test(phoneStr));
+
